24a8ff4ed2fea073b04ff74e36c903a5f09a80ab
Tyler Dempsey <tylerdempsey1@hotmail.com>
2018-09-03 02:24:21 +0000

    automatically backed up by learn

---
+++ functions.js
@@ -0,0 +1,58 @@
+function doNothing() {}
+
+doNothing()
+
+function sayHello() {
+  console.log("Hello!")
+}
+
+sayHello()
+
+function sayHelloToIsabel(){
+  console.log("Hello, Isbel!")
+}
+
+sayHelloToIsabel()
+
+function sayHelloToSofia() {
+  console.log("Hello, Sofia!")
+}
+
+sayHelloToSofia()
+
+function sayHelloToBrandon(){
+  console.log("Hello, Brandon!")
+  
+}
+
+sayHelloToBrandon()
+
+function doSomething(thing) {
+  console.log(thing)
+}
+
+doSomething("anything")
+
+function sayHelloTo(firstName){
+  console.log(`Hello, ${firstName}!`)
+}
+
+sayHelloTo("Isbel")
+sayHelloTo("Jane")
+sayHelloTo("R2-D2")
+sayHelloTo(1)
+
+
+function say(greeting, firstName){
+  return `${greeting}, ${firstName}`
+  }
+  
+  say("Good Morning", "Tim")
+  
+function add(x, y) {
+  return x + y
+}
+
+add(1, 2)
+
+
---
+++ test.js
@@ -0,0 +1,31 @@
+
+
+word = "bird";
+
+console.log(word);
+
+word = "dog";
+
+console.log(word);
+
+
+
+var a = 5, b = 2, c = 3, d = "hello", e = "goodbye";
+
+console.log(d);
+
+console.log(b, c);
+
+var myNote = "get milk"
+
+console.log(myNote)
+
+myNote = "Get Cookies"
+
+console.log(myNote)
+
+function say(greeting, firstName) {
+  console.log('I was called!')
+  return (`${greeting}, ${firstName}!`)
+}
+say("hello", "tim")
